bf857a74f1f526cf2b05e6a4bfbb193855ba098d
Chad King <chad.king@ebrokersoftware.com>
2015-02-11 17:00:06 -0700

    added some additional testing

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,16 +1,27 @@
 describe('main controller', function(){
-  var mockToast, mockService
+  var mockToast, mockService, q, deferred;
   beforeEach(function(){
 
     module('chadJiffDemo.sign-in.controller', function($provide){
       $provide.value('$mdToast', mockToast);
-      mockService = {connectInstagram: jasmine.createSpy()};
-      $provide.value('instagramService', mockService);
+      mockService = {
+        isReady: function(){
+          return jasmine.createSpy();
+        },
+        connectInstagram: function(){
+        deferred  = q.defer();
+        return deferred.promise;
+        }
+      };
       $provide.value('instagramService', mockService);
     })
 
-    inject(function($controller, _$rootScope_){
+    inject(function($controller, _$rootScope_, $q){
       $scope = {};
+      q = $q;
+      $rootScope = _$rootScope_;
+      spyOn($rootScope, '$broadcast').and.callThrough();
+      spyOn(mockService, 'connectInstagram').and.callThrough();
       $rootscope = _$rootScope_;
       $controller('MainCtrl', {$scope: $scope})
     })
@@ -20,5 +31,38 @@ describe('main controller', function(){
   it('should define getLoggedIn on scope as a function', function(){
     expect($scope.getLoggedIn).toBeDefined();
     expect($scope.getLoggedIn).toEqual(jasmine.any(Function));
+  });
+
+  describe('$scope.getLoggedIn with isReady true', function(){
+    beforeEach(function(){
+      $scope.getLoggedIn();
+      spyOn(mockService, 'isReady').and.returnValue(true)
+      deferred.resolve();
+      $rootScope.$digest();
+    });
+      it('should call instagramService.contectInstagram', function(){
+        expect(mockService.connectInstagram).toHaveBeenCalled();
+      });
+
+      it('should call instagramService.isReady', function(){
+        expect(mockService.isReady).toHaveBeenCalled();
+      });
+
+      it('should call rootScope.broadcast', function(){
+        expect($rootScope.$broadcast).toHaveBeenCalled();
+      })
+  });
+
+  describe('$scope.getLoggedIn with isReady false', function(){
+    beforeEach(function(){
+      $scope.getLoggedIn();
+      spyOn(mockService, 'isReady').and.returnValue(false)
+      deferred.resolve();
+      $rootScope.$digest();
+    })
+    it('should call isReady and not call $rootScope.$broadcast', function(){
+      expect(mockService.isReady).toHaveBeenCalled();
+      expect($rootScope.$broadcast).not.toHaveBeenCalled();
+    })
   })
 });
